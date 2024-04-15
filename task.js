fff1cb7cd8e7f507ef988b4a74dce5fa579ca5f7
garfild <garfild13-06@yandex.com>
2024-04-15 12:08:08 +0300

    Done via cookie

--- task.js
+++ task.js
@@ -1,11 +1,33 @@
+function getCookie(name) {
+    try {
+        const pairs = document.cookie.split('; ')
+        const cookie = pairs.find(p => p.startsWith(name + '='))
+        return cookie.substring(name.length + 1)
+    } catch (e) {
+        return null
+    }
+
+}
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + encodeURIComponent(value)
+}
+
+function removeCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+}
+
+
 const signin = document.querySelector('.signin');
 const signinForm = document.forms[0];
 document.querySelector('.quit').addEventListener('click', () => {
-    localStorage.clear()
+    removeCookie('user_id')
+    // localStorage.clear()
     location.reload()
 })
 
-const user_id = localStorage.getItem('user_id')
+// const user_id = localStorage.getItem('user_id')
+const user_id = getCookie('user_id')
 if (user_id != null) {
     signin.classList.remove('signin_active');
     const welcome = document.querySelector('.welcome');
@@ -29,7 +51,8 @@ if (user_id != null) {
                     const welcome = document.querySelector('.welcome');
                     welcome.classList.add('welcome_active');
                     welcome.querySelector('#user_id').innerText = response['user_id'];
-                    localStorage.setItem('user_id', response['user_id'])
+                    // localStorage.setItem('user_id', response['user_id'])
+                    setCookie('user_id', response['user_id'])
                 } else {
                     login.value = '';
                     pass.value = ''
